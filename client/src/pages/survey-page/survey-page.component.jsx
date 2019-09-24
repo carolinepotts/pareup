f49923a4b08194dedc87990a038f5587eae0a337
brookezkeene <bzk2@duke.edu>
2019-09-24 00:34:06 -0400

    form UI created

--- client/src/pages/survey-page/survey-page.component.jsx
+++ client/src/pages/survey-page/survey-page.component.jsx
@@ -18,107 +18,177 @@ class SurveyPage extends React.Component {
             intervalIsSet: false,
             ed_levels: ED_LEVELS,
             job_titles: JOB_TITLES,
-            negotiated: NEGOTIATED,
             company_sizes: COMPANY_SIZES,
-            radius: 50,
-            zipcode_filter: 80230,
             valid_zips: [],
-            data: []
+            data: [],
+            job_title: "",
+            company_size: "",
+            salary: "",
+            equity: "",
+            negotiated: "",
+            one_time: "",
+            lat: "",
+            long: ""
         }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    
-    // our put method that uses our backend api
-    // to create new query into our data base
-    putDataToDB = ({ location, salary, internships, equity, signing_bonus }) => {
-        console.log(location);
-        let currentIds = this.state.data.map((data) => data.id);
-        let idToBeAdded = 0;
-        while (currentIds.includes(idToBeAdded)) {
-            ++idToBeAdded;
-        }
+    handleChange(event) {
+        const {name, value} = event.target;
 
+        this.setState(() => ({
+            [name]: value
+        }))
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        
         axios.post('http://localhost:3001/api/putData', {
-            location: location,
+            job_title: job_title,
+            company_size: company_size,
             salary: salary,
-            internships: internships,
             equity: equity,
-            signing_bonus: signing_bonus
+            negotiated: negotiated,
+            one_time: one_time,
+            lat: lat,
+            long: long
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-    };
+    }
+
 
     render() {
         return (
             <div className='App'>
-                <h1>Survey Page - Enter your offer info here!</h1>
-                <Form className='filter-list'>
-                    <Form.Row>
-                        <Col>
-                            <Form.Label>Education Level</Form.Label>
-                            <DropdownList className="filter" data={ED_LEVELS} textField='name'
-                                placeholder='All Education Levels'
-                                valueField='name'
-                                onChange={value => this.setState((value.length > 0) ?
-                                    { ed_levels: value } : { ed_levels: ED_LEVELS }, () => console.log(this.state))} />
-                        </Col>
-                        <Col>
-                            <Form.Label>Job Title</Form.Label>
-                            <DropdownList className="filter" data={JOB_TITLES} textField='name'
-                                placeholder='All Job Titles'
-                                valueField='name'
-                                onChange={value => this.setState((value.length > 0) ?
-                                    { job_titles: value } : { job_titles: JOB_TITLES }, () => console.log(this.state))} />
-                        </Col>
-                    </Form.Row>
-                    <Form.Row>
-                        <Col>
-                            <Form.Label>Negotiation Status</Form.Label>
-                            <DropdownList className="filter" data={NEGOTIATED} textField='name'
-                                placeholder='All Negotiation Statuses'
-                                valueField='value'
-                                onChange={value => this.setState((value.length > 0) ?
-                                    { negotiated: value } : { negotiated: NEGOTIATED }, () => console.log(this.state))} />
-                        </Col>
-                        <Col>
-                            <Form.Label>Company Size</Form.Label>
-                            <DropdownList className="filter" data={COMPANY_SIZES} textField='name'
-                                placeholder='All Company Sizes'
-                                valueField='name'
-                                onChange={value => this.setState((value.length > 0) ?
-                                    { company_sizes: value } : { company_sizes: COMPANY_SIZES }, () => console.log(this.state))} />
-                        </Col>
-                    </Form.Row>
-                    <Form.Row style={{ marginTop: '5px' }}>
-                        <Col>
-                            <Form.Label className="mt-auto">Show companies within</Form.Label>
-                            <input
-                                className="mt-auto"
-                                type="text"
-                                style={{ width: '40px', marginLeft: '10px', marginRight: '10px', borderRadius: '5px', borderWidth: '1px', height: '45px', textAlign: 'center' }}
-                                onChange={(e) => this.setState({ radius: e.target.value })}
-                                placeholder="#"
-                            />
+                <h1>Submit Your Offer</h1>
+                <p>We understand that talking about money and salaries is sometimes considered taboo, but the more transparent we become about our offers, the more fair and equal will compensation become.</p>
+                <h4>Thank you for your help!</h4>
 
-                            <Form.Label>miles of the zipcode</Form.Label>
-                            <input
-                                className="mt-auto"
-                                type="text"
-                                style={{ width: '100px', marginLeft: '10px', marginRight: '10px', borderRadius: '5px', borderWidth: '1px', height: '45px', textAlign: 'center' }}
-                                onChange={(e) => this.setState({ zipcode_filter: e.target.value })}
-                                placeholder="zipcode"
-                            />
-                        </Col>
-                        <Col>
-                            <Button onClick={(e) => {
-                                e.preventDefault();
-                                this.findZipCodes(this.state.zipcode_filter, this.state.radius).then(console.log("finished"))
-                            }}
-                                style={{ backgroundColor: '#1d60b8', borderColor: '#1d60b8', fontSize: '20px' }}>
-                                Submit Offer Information
-                                    </Button>
-                        </Col>
-                    </Form.Row>
-                </Form>
+                <div className="form-container">
+                    <Form className='filter-list'>
+                        <h3>Company Information (Required)</h3>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>Company Location</Form.Label>
+                                <br></br>
+                                <input class="text-input" placeholder="City, State (i.e. Seattle, WA)" required="true"/>
+                                <br></br>
+                            </Col>
+                            <Col>
+                                <Form.Label>Company Size</Form.Label>
+                                <br></br>
+                                <div class='btn-group'>
+                                    <button>&#60; 100</button>
+                                    <button>100-1,000</button>
+                                    <button>1,000-10,000</button>
+                                    <button>10,000+</button>
+                                </div>
+                                <br></br>
+                            </Col>
+                        </Form.Row>
+                        <br></br>
+                        <h3>Compensation Information (Required)</h3>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>Job Title</Form.Label>
+                                <DropdownList className="filter" data={JOB_TITLES} textField='name'
+                                    placeholder='All Job Titles'
+                                    valueField='name'
+                                    onChange={value => this.setState((value.length > 0) ?
+                                        { job_titles: value } : { job_titles: JOB_TITLES }, () => console.log(this.state))} />
+
+                                <Form.Label>Base Salary</Form.Label>
+                                <br></br>
+                                <input class="number-input" placeholder="$ Yearly Salary (i.e. XXXXXX)" required="true"/>
+                                <br></br>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <Form.Label>Bonus</Form.Label>
+                                        <br></br>
+                                        <input class="number-input" placeholder="(i.e. XXXX)" required="true"/>
+                                        <br></br>
+                                    </Col>
+
+                                    <Col>
+                                        <Form.Label>Equity</Form.Label>
+                                        <br></br>
+                                        <input class="percent-input" placeholder="(i.e. X.XX)" required="true"/>
+                                        <br></br>
+                                    </Col>
+                                </Row>
+                                
+                                <br></br>
+                                <Form.Label>Did you negotiate this offer?</Form.Label>
+                                <br></br>
+                                <div class="btn-group">
+                                    <button>Yes</button>
+                                    <button>No</button>
+                                </div>
+                                <br></br>
+                            </Col>
+                        </Form.Row>
+                        <br></br>
+                        <h3>Personal Information (Optional)</h3>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>School</Form.Label>
+                                <br></br>
+                                <input class="text-input" placeholder ="School or University Name" required="false"/>
+                                <br></br>
+
+                                <Form.Label>Education Level</Form.Label>
+                                <DropdownList className="filter" data={ED_LEVELS} textField='name'
+                                    placeholder='All Education Levels'
+                                    valueField='name'
+                                    onChange={value => this.setState((value.length > 0) ?
+                                        { ed_levels: value } : { ed_levels: ED_LEVELS }, () => console.log(this.state))} />
+
+                                <Form.Label>Field of Study</Form.Label>
+                                <br></br>
+                                <input class="text-input" placeholder="i.e. Computer Science, Engineering etc." required="false" />
+                                <br></br>
+                            </Col>
+                            <Col>
+                                <Form.Label>Personal Pronouns</Form.Label>
+                                <DropdownList className="filter" data={COMPANY_SIZES} textField='name'
+                                    placeholder='i.e. she/her, he/him, they/them etc.'
+                                    valueField='name'
+                                    onChange={value => this.setState((value.length > 0) ?
+                                        { company_sizes: value } : { company_sizes: COMPANY_SIZES }, () => console.log(this.state))} />
+
+                                <Form.Label>Do you identify as Hispanic or Latinx?</Form.Label>
+                                <br></br>
+                                <div class="btn-group">
+                                    <button>Yes</button>
+                                    <button>No</button>
+                                </div>
+                                <br></br>
+
+                                <Form.Label>Race</Form.Label>
+                                <DropdownList className="filter" data={COMPANY_SIZES} textField='name'
+                                    placeholder='i.e. White, African American, Asian etc.'
+                                    valueField='name'
+                                    onChange={value => this.setState((value.length > 0) ?
+                                        { company_sizes: value } : { company_sizes: COMPANY_SIZES }, () => console.log(this.state))} />
+                            </Col>
+                        </Form.Row>
+                        <Form.Row style={{ marginTop: '5px' }}>
+                            <Button class="main-btn" align="center" onClick={this.handleSubmit}>
+                            Submit
+                            </Button>
+                        </Form.Row>
+                    </Form>
+                </div>
             </div>
         )
     }
