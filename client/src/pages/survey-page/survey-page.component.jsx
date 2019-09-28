fca0ac9b70c091f2a8d3303b63d87061bfe061c8
brookezkeene <bzk2@duke.edu>
2019-09-28 13:41:24 -0400

    handling changes somewhat, recloning new master

--- client/src/pages/survey-page/survey-page.component.jsx
+++ client/src/pages/survey-page/survey-page.component.jsx
@@ -19,8 +19,6 @@ class SurveyPage extends React.Component {
             ed_levels: ED_LEVELS,
             job_titles: JOB_TITLES,
             company_sizes: COMPANY_SIZES,
-            valid_zips: [],
-            data: [],
             job_title: "",
             company_size: "",
             salary: "",
@@ -28,11 +26,19 @@ class SurveyPage extends React.Component {
             negotiated: "",
             one_time: "",
             lat: "",
-            long: ""
+            long: "",
+            ed_level: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDropDown = this.handleDropDown.bind(this);
+    }
+
+    handleDropDown(event) {
+        this.setState((event.target.value.length > 0) ? { ed_levels: event.target.value } : { ed_levels: ED_LEVELS })
+
+        console.log(this.state)
     }
 
     handleChange(event) {
@@ -41,20 +47,22 @@ class SurveyPage extends React.Component {
         this.setState(() => ({
             [name]: value
         }))
+
+        console.log(this.state)
     }
 
     handleSubmit(event) {
         event.preventDefault();
         
         axios.post('http://localhost:3001/api/putData', {
-            job_title: job_title,
-            company_size: company_size,
-            salary: salary,
-            equity: equity,
-            negotiated: negotiated,
-            one_time: one_time,
-            lat: lat,
-            long: long
+            job_title: this.state.job_title,
+            company_size: this.state.company_size,
+            salary: this.state.salary,
+            equity: this.state.equity,
+            negotiated: this.state.negotiated,
+            one_time: this.state.one_time,
+            lat: this.state.lat,
+            long: this.statelong
         })
         .then(function (response) {
             console.log(response);
@@ -79,14 +87,15 @@ class SurveyPage extends React.Component {
                             <Col>
                                 <Form.Label>Company Location</Form.Label>
                                 <br></br>
-                                <input class="text-input" placeholder="City, State (i.e. Seattle, WA)" required="true"/>
+                                <input class="text-input" placeholder="City, State (i.e. Seattle, WA)" required="true" onChange={value => this.setState((value.length > 0) ?
+                                        { ed_levels: value } : { ed_levels: ED_LEVELS }, () => console.log(this.state))} />
                                 <br></br>
                             </Col>
                             <Col>
                                 <Form.Label>Company Size</Form.Label>
                                 <br></br>
                                 <div class='btn-group'>
-                                    <button>&#60; 100</button>
+                                    <button name="company_size" value={this.state.company_size} onClick={this.handleChange}>&#60; 100</button>
                                     <button>100-1,000</button>
                                     <button>1,000-10,000</button>
                                     <button>10,000+</button>
@@ -98,19 +107,11 @@ class SurveyPage extends React.Component {
                         <h3>Compensation Information (Required)</h3>
                         <Form.Row>
                             <Col>
-                                <Form.Label>Job Title</Form.Label>
-                                <DropdownList className="filter" data={JOB_TITLES} textField='name'
-                                    placeholder='All Job Titles'
-                                    valueField='name'
-                                    onChange={value => this.setState((value.length > 0) ?
-                                        { job_titles: value } : { job_titles: JOB_TITLES }, () => console.log(this.state))} />
-
                                 <Form.Label>Base Salary</Form.Label>
                                 <br></br>
-                                <input class="number-input" placeholder="$ Yearly Salary (i.e. XXXXXX)" required="true"/>
+                                <input class="number-input" placeholder="$ Yearly Salary (i.e. XXXXXX)" required="true" name='salary' onChange={this.handleChange}/>
                                 <br></br>
-                            </Col>
-                            <Col>
+                                <br></br>
                                 <Row>
                                     <Col>
                                         <Form.Label>Bonus</Form.Label>
@@ -136,32 +137,29 @@ class SurveyPage extends React.Component {
                                 </div>
                                 <br></br>
                             </Col>
+                            <Col>
+                                <Form.Label>Job Title</Form.Label>
+                                <DropdownList className="filter" data={JOB_TITLES} name='job_title' textField='name'
+                                    placeholder='All Job Titles'
+                                    valueField='name'
+                                    onChange={value => this.setState((value.length > 0) ?
+                                        { job_titles: value } : { job_titles: JOB_TITLES }, () => this.handleChange)} />
+
+                                <Form.Label>Education Level</Form.Label>
+                                <DropdownList className="filter" data={ED_LEVELS} textField='name'
+                                    name='ed_level'
+                                    placeholder='All Education Levels'
+                                    valueField='name'
+                                    onChange={this.handleChange} />
+                            </Col>
                         </Form.Row>
                         <br></br>
                         <h3>Personal Information (Optional)</h3>
                         <Form.Row>
                             <Col>
-                                <Form.Label>School</Form.Label>
-                                <br></br>
-                                <input class="text-input" placeholder ="School or University Name" required="false"/>
-                                <br></br>
-
-                                <Form.Label>Education Level</Form.Label>
-                                <DropdownList className="filter" data={ED_LEVELS} textField='name'
-                                    placeholder='All Education Levels'
-                                    valueField='name'
-                                    onChange={value => this.setState((value.length > 0) ?
-                                        { ed_levels: value } : { ed_levels: ED_LEVELS }, () => console.log(this.state))} />
-
-                                <Form.Label>Field of Study</Form.Label>
-                                <br></br>
-                                <input class="text-input" placeholder="i.e. Computer Science, Engineering etc." required="false" />
-                                <br></br>
-                            </Col>
-                            <Col>
-                                <Form.Label>Personal Pronouns</Form.Label>
+                                <Form.Label>Race</Form.Label>
                                 <DropdownList className="filter" data={COMPANY_SIZES} textField='name'
-                                    placeholder='i.e. she/her, he/him, they/them etc.'
+                                    placeholder='i.e. White, African American, Asian etc.'
                                     valueField='name'
                                     onChange={value => this.setState((value.length > 0) ?
                                         { company_sizes: value } : { company_sizes: COMPANY_SIZES }, () => console.log(this.state))} />
@@ -173,16 +171,18 @@ class SurveyPage extends React.Component {
                                     <button>No</button>
                                 </div>
                                 <br></br>
-
-                                <Form.Label>Race</Form.Label>
+                            </Col>
+                            <Col>
+                                <Form.Label>Personal Pronouns</Form.Label>
                                 <DropdownList className="filter" data={COMPANY_SIZES} textField='name'
-                                    placeholder='i.e. White, African American, Asian etc.'
+                                    placeholder='i.e. she/her, he/him, they/them etc.'
                                     valueField='name'
                                     onChange={value => this.setState((value.length > 0) ?
                                         { company_sizes: value } : { company_sizes: COMPANY_SIZES }, () => console.log(this.state))} />
                             </Col>
+
                         </Form.Row>
-                        <Form.Row style={{ marginTop: '5px' }}>
+                        <Form.Row style={{ marginTop: '50px' }}>
                             <Button class="main-btn" align="center" onClick={this.handleSubmit}>
                             Submit
                             </Button>
